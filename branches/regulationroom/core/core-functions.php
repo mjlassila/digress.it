b3696451b47bfb59b0cba2afe54185e5cb7a4bbc
rebecca.younes <rebecca.younes@b80beb6a-a484-11de-b98f-4500b42dc123>
2012-01-30 19:43:09 +0000

    RR-395 Remove hard-coded references to wp_capabilities, to make plugin table-prefix-agnostic

--- branches/regulationroom/core/core-functions.php
+++ branches/regulationroom/core/core-functions.php
@@ -1268,7 +1268,7 @@ function get_root_domain(){
 	$testing_mode = false;
 	$production_mode = false;
 
-	return "http://" . DOMAIN_CURRENT_SITE; //isset($_SERVER['HTTPS']) ? "https://". DOMAIN_CURRENT_SITE : "http://" . DOMAIN_CURRENT_SITE;
+	return site_url();
 }
 
 

7d4e1157a86b9e2e3a1e463ba973038e083f966d
rebecca.younes <rebecca.younes@b80beb6a-a484-11de-b98f-4500b42dc123>
2012-03-09 20:15:14 +0000

    Fix blank_with_logo template after header background images changed to img tags

--- branches/regulationroom/themes/digressit-wireframe/blank_with_logo.php
+++ branches/regulationroom/themes/digressit-wireframe/blank_with_logo.php
@@ -12,7 +12,7 @@ Template Name: Blank With Logo
 <title><?php wp_title(''); ?></title>
 <?php 
 get_currentuserinfo(); 
-// Can we remove the framework? What's it used for?
+
 remove_action('wp_head', '_ak_framework_meta_tags');
 // There are other things we don't need here. Use remove_action calls to get rid of them.
 wp_head(); 
@@ -21,9 +21,15 @@ wp_head();
 
 <body>
 
-<?php 
-do_action('add_header_image');
-?>
+<?php do_action('optional_pre_header'); ?>
+<header id="header" role="header">
+    <div class="site-title" role="banner">
+        <div class="bubblearrow"></div>     
+        <?php if(has_action('add_header_image')): ?>
+            <?php do_action('add_header_image'); ?>
+        <?php endif; ?>
+    </div>
+</header>
 <div style="clear:both;"></div>
 <?php
 while ( have_posts() ) {

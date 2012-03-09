01193107641aeb77e536940dbe3dc07e2877a0d1
rebecca.younes <rebecca.younes@b80beb6a-a484-11de-b98f-4500b42dc123>
2012-03-09 19:58:02 +0000

    Add a truly blank template, and rename previous blank template to blank_with_logo

--- branches/regulationroom/themes/digressit-wireframe/blank_with_logo.php
+++ branches/regulationroom/themes/digressit-wireframe/blank_with_logo.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Blank
+Template Name: Blank With Logo
 */
 ?>
 
@@ -21,6 +21,10 @@ wp_head();
 
 <body>
 
+<?php 
+do_action('add_header_image');
+?>
+<div style="clear:both;"></div>
 <?php
 while ( have_posts() ) {
     the_post(); 
